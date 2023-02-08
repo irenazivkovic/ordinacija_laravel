54c0a72f7a45198a3242a829f9a644172baa1a85
Irena Zivkovic <zivkovicirena3@gmail.com>
2023-02-08 15:19:01 +0100

    api rute i kontroleri

---
+++ ordinacija/app/Http/Controllers/ZubarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Zubar;
+use Illuminate\Http\Request;
+
+class ZubarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Zubar  $zubar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Zubar $zubar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Zubar  $zubar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Zubar $zubar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Zubar  $zubar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Zubar $zubar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Zubar  $zubar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Zubar $zubar)
+    {
+        //
+    }
+}
--- ordinacija/routes/api.php
+++ ordinacija/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\KlijentController;
+use App\Http\Controllers\ZubarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('zubar', [ZubarController::class, 'index']);
+Route::get('zubar/{zubar}', [ZubarController::class, 'show']);
+Route::post('zubar', [ZubarController::class, 'store']);
+Route::delete('zubar/{zubar}', [ZubarController::class, 'destroy']);
+Route::get('klijent', [KlijentController::class, 'index']);
+Route::get('klijent/{klijent}', [KlijentController::class, 'show']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
